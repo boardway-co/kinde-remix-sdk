f4e2944e21275d2a497ce4e62e12ce30a4ca594e
Juraj Misur <juraj.misur@gmail.com>
2025-02-18 17:28:07 +0100

    chore: release v1.1.3-headers-2

--- dist/types/session/session.d.ts
+++ dist/types/session/session.d.ts
@@ -1,6 +1,6 @@
 export function createSessionManager(request: Request): Promise<{
-  cookies: Cookies;
-  sessionManager: import("@kinde-oss/kinde-typescript-sdk").SessionManager;
+    cookies: Cookies;
+    sessionManager: import("@kinde-oss/kinde-typescript-sdk").SessionManager;
 }>;
 import Cookies from "universal-cookie";
 //# sourceMappingURL=session.d.ts.map
